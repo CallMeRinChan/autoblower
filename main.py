ace09934c02340663f096f3c634e8fe55f284b82
CallMeRinChan <call0me0rin0chan@gmail.com>
2022-05-03 23:29:07 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# the biggest mistake ill ever make, i want this code to automatically play the game "Leaf blower Revolution Idle"
+# made by CallMeRinChan and Zeehondie
+
+#insert tensorflow stuff
+import tensorflow
